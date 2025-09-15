c3fef32b1ee3464d221f6c4b7a443e58c15c29e0
Armaan Mishra <armaanmishra48@gmail.com>
2025-09-15 08:14:55 +0530

    add exoplayer component in video screen and refactor code

--- screens/VideoPlayerScreen.tsx
+++ screens/VideoPlayerScreen.tsx
@@ -1,33 +1,37 @@
-//bonus features-
-//1. add a progress bar
-//2. add a volume control
-//3. continue watching from where you left off
-//4. favorites tab and home tab
-
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Platform, StatusBar, Text, View } from 'react-native';
 import { mockData } from '../assets/mockData';
-import ExoPlayer2 from '../specs/ExoPlayer2';
+import ExoPlayer from '../specs/ExoPlayer';
 import { VideoPlayerScreenProps } from '../types';
 
-export default function VideoPlayerScreen({ route }: VideoPlayerScreenProps) {
-  const { itemTitle } = route.params;
-  const vidSource = mockData.find(item => item.title === itemTitle)?.videoUrl;
+export default function VideoPlayerScreen({
+  route,
+  navigation,
+}: VideoPlayerScreenProps) {
+  const handleClose = () => {
+    navigation.goBack();
+  };
+  const { itemTitle, itemId, startTime } = route.params;
+  const vidSource = mockData.find(item => item.id === itemId)?.videoUrl;
+
+  if (Platform.OS === 'ios') {
+    return (
+      <View className="flex-1 bg-black h-full w-full ">
+        <StatusBar hidden={true} />
+        <Text>Placeholder for iOS</Text>
+      </View>
+    );
+  }
+
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-zinc-900 gap-2 w-full h-full">
-      {/* <ExoPlayerTest /> */}
-      {/* <Text className="text-white">Now playing- {itemTitle}</Text>
-      <Button
-        onPress={() => {
-          navigation.goBack();
-        }}
-      >
-        Go back
-      </Button> */}
-      <ExoPlayer2
+    <View className="flex-1 bg-black h-full w-full ">
+      <StatusBar hidden={true} />
+      <ExoPlayer
         source={vidSource}
-        // eslint-disable-next-line react-native/no-inline-styles
-        style={{ height: 300, width: 390 }}
+        title={itemTitle}
+        itemId={itemId}
+        handleClose={handleClose}
+        startTime={startTime}
       />
-    </SafeAreaView>
+    </View>
   );
 }
