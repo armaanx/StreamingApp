2b1cef96dc46a04f749d5547cc42d74b07196e6f
Armaan Mishra <armaanmishra48@gmail.com>
2025-09-15 08:10:03 +0530

    update types

--- types.ts
+++ types.ts
@@ -3,7 +3,12 @@ import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 export type RootStackParamList = {
   Home: undefined;
   Details: { itemId: string };
-  Video: { itemId: string; itemTitle: string; videoUrl: string };
+  Video: {
+    itemId: string;
+    itemTitle: string;
+    videoUrl: string;
+    startTime?: number;
+  };
 };
 
 export type HomeScreenProps = NativeStackScreenProps<
@@ -19,3 +24,5 @@ export type VideoPlayerScreenProps = NativeStackScreenProps<
   RootStackParamList,
   'Video'
 >;
+
+export type ContinueWatchingItem = CatalogueItemType & { progress: number };
