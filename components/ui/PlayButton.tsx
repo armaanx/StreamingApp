4bdac4846f2af70e293f9cf4128d670c5729a5ed
Armaan Mishra <armaanmishra48@gmail.com>
2025-09-15 08:12:38 +0530

    refactor play button to make it reusable with custon title and loading state

--- components/ui/PlayButton.tsx
+++ components/ui/PlayButton.tsx
@@ -1,18 +1,31 @@
 import { Play } from 'lucide-react-native';
-import { Pressable, Text } from 'react-native';
+import { ActivityIndicator, Pressable, Text } from 'react-native';
 
 interface PlayButtonProps {
+  label: string;
   onPress: () => void;
+  loading?: boolean;
 }
 
-export default function PlayButton({ onPress }: PlayButtonProps) {
+export default function PlayButton({
+  onPress,
+  label,
+  loading,
+}: PlayButtonProps) {
   return (
     <Pressable
       onPress={onPress}
       className="flex-row gap-2 items-center justify-center bg-zinc-200 h-14 w-full rounded-lg active:bg-zinc-400"
+      disabled={loading}
     >
-      <Play size={22} fill={'black'} />
-      <Text className="text-black font-bold text-xl">Play</Text>
+      {!loading ? (
+        <>
+          <Play size={22} fill={'black'} />
+          <Text className="text-black font-bold text-xl">{label}</Text>
+        </>
+      ) : (
+        <ActivityIndicator size={'small'} color={'black'} />
+      )}
     </Pressable>
   );
 }
