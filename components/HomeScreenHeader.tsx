969d9b76fde7fe23c773e506e7ac4d8bb4d6dcf5
Armaan Mishra <armaanmishra48@gmail.com>
2025-09-15 08:10:56 +0530

    refactor home screen header to add profile and settings icon

--- components/HomeScreenHeader.tsx
+++ components/HomeScreenHeader.tsx
@@ -1,9 +1,14 @@
+import { Settings, UserRoundPen } from 'lucide-react-native';
 import { Text, View } from 'react-native';
 
 export default function HomeScreenHeader() {
   return (
-    <View className="flex-row items-center justify-center bg-zinc-900 h-20 w-full drop-shadow-sm border-b border-zinc-950">
-      <Text className="text-white font-bold text-xl">MovieApp</Text>
+    <View className="flex-row items-center justify-between px-5 bg-zinc-900 h-20 w-full drop-shadow-sm border-b border-zinc-950">
+      <Text className="text-white font-bold text-xl">Streaming App</Text>
+      <View className="flex-row items-center gap-4">
+        <Settings size={24} color={'white'} />
+        <UserRoundPen size={24} color={'white'} />
+      </View>
     </View>
   );
 }
